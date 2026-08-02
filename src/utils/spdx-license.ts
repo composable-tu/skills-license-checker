8d0894d5621fe6dcc8109a78b0fbce4124a336f3
行袂開跤 <208613607+composable-tu@users.noreply.github.com>
2026-08-03 01:56:05 +0800

    docs(license): 更新文件头注释中的项目名称
    - 修正 src/config/agent-dirs.ts 中的许可证声明
    - 修正 src/utils/find-license.ts 中的许可证声明
    - 修正 src/utils/find-skill.ts 中的许可证声明
    - 修正 src/index.ts 中的许可证声明
    - 修正 tests/main.test.ts 中的许可证声明
    - 修正 src/utils/parse-skill-front.ts 中的许可证声明
    - 修正 src/utils/parse-vercel-skills-lock.ts 中的许可证声明
    - 修正 src/utils/spdx-license.ts 中的许可证声明

--- src/utils/spdx-license.ts
+++ src/utils/spdx-license.ts
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2026 Skills License Checker
- * SM2 Key Generator is licensed under Mulan PSL v2.
+ * Skills License Checker is licensed under Mulan PSL v2.
  * You can use this software according to the terms and conditions of the Mulan PSL v2.
  * You may obtain a copy of Mulan PSL v2 at:
  *          http://license.coscl.org.cn/MulanPSL2
